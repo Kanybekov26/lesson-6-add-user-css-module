60ae7b365cf75b98730c89d19ddd450c4c35d7a8
bekbol <kanybekuulubekbol26@gamil.comgit config --global user.email kanybekuulubekbol26@gmail.com>
2023-01-17 13:09:47 +0600

    test

--- src/App.js
+++ src/App.js
@@ -3,53 +3,45 @@ import "./App.css";
 import AgeForm from "./components/ageform/AgeForm";
 import NewAgeForm from "./components/newageform/NewAgeForm";
 
-
- 
 function App() {
-  const [yearUser,setYearUser] = useState( [
+  const [yearUser, setYearUser] = useState([
     {
       name: "Feruza",
       age: 19,
     },
     {
-    name: "Bayaman",
-    age: 90,
-  },
-  {
-    name: "Nurbolot",
-    age: 25,
-  },
-  {
-    name: "Zhumabek",
-    age: 24,
-  },
-  {
-    name: "Zhasmina",
-    age: 22,
-  },
-  
-  ])
+      name: "Bayaman",
+      age: 90,
+    },
+    {
+      name: "Nurbolot",
+      age: 25,
+    },
+    {
+      name: "Zhumabek",
+      age: 24,
+    },
+    {
+      name: "Zhasmina",
+      age: 22,
+    },
+  ]);
 
-  yearUser.sort((a,b) => {
-  return  b.age-a.age
-  })
+  yearUser.sort((a, b) => {
+    return b.age - a.age;
+  });
 
-  
   const newyearuser = (ageuser) => {
     console.log(ageuser);
-    const copiyearUser = [...yearUser] 
-    copiyearUser.push(ageuser)
-    setYearUser(copiyearUser)
-
-  }
+    const copiyearUser = [...yearUser];
+    copiyearUser.push(ageuser);
+    setYearUser(copiyearUser);
+  };
   return (
     <div className="App">
-      <NewAgeForm 
-      newyearuser={newyearuser}
-      />
-      <AgeForm 
-      yearuser={yearUser}
-      />
+      <NewAgeForm newyearuser={newyearuser} />
+      test
+      <AgeForm yearuser={yearUser} />
     </div>
   );
 }
